172f71d426ab099f5110b82e36d8d77bcc00b8a5
opdoffer <rduivesteijn@gmail.com>
2016-11-30 23:41:15 +0100

    update

--- inst-apache2.sh
+++ inst-apache2.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 ### CBL script installing Apache2
 
+echo -e "Webserver wordt geinstalleerd"
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install apache -y
 sudo apt-get install apache2 apache2-utils -y
 
-sudo su -c 'cat <<EOF >  /etc/apache2/sites-enabled/000-default.conf
-<VirtualHost *:80>
-    ServerAdmin webmaster@localhost
-    DocumentRoot /var/www/html
-    ErrorLog ${APACHE_LOG_DIR}/error.log
-    CustomLog ${APACHE_LOG_DIR}/access.log combined
+echo -e "De website bestanden worden gekopieerd"
+sudo chmod -R 0777 /var/www/html
+sudo cp -r /cbl-web/www/* /var/www/html
 
-    <Directory "/var/www/html">
-        AuthType Basic
-        AuthName "Restricted Content"
-        AuthUserFile /etc/apache2/.htpasswd
-        Require valid-user
-    </Directory>
-</VirtualHost>
-EOF'
+echo -e " "
 
-cp -r /cbl/www/* /var/www/html
 
---
+++ wachtwoord-op-site.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Zorg dat de website om een wachtwoord vraagt
+
+echo -e "Dit commando gaat zorgen dat je website altijd om een wachtwoord gaat vragen om in te loggen."
+
+sudo su -c 'cat <<EOF >  /etc/apache2/sites-enabled/000-default.conf
+<VirtualHost *:80>
+    ServerAdmin webmaster@localhost
+    DocumentRoot /var/www/html
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+    <Directory "/var/www/html">
+        AuthType Basic
+        AuthName "Restricted Content"
+        AuthUserFile /etc/apache2/.htpasswd
+        Require valid-user
+    </Directory>
+</VirtualHost>
+EOF'
+
+echo -e "Klaar!"
