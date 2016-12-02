e12ed6299d8f59b62dce8e927e7288b692c45d7e
opdoffer <rduivesteijn@gmail.com>
2016-12-02 09:56:05 +0100

    update

--- web.sh
+++ web.sh
@@ -3,7 +3,7 @@
 
 echo -e "De website bestanden worden gekopieerd"
 sudo chmod -R 0777 /var/www/html
-sudo cp -r /cbl-web/www/* /var/www/html
+sudo cp -r ./cbl-web/www/* /var/www/html
 
 echo -e " "
 
