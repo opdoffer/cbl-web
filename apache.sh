bb65d745f261ff07308652c3b086d9df2a8a46f9
opdoffer <rduivesteijn@gmail.com>
2016-12-02 09:19:46 +0100

    update

--- apache.sh
+++ apache.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 ### CBL script installing Apache2
 
-echo -e "Webserver wordt geinstalleerd"
+echo -e "Webserver en alle extra's worden geïnstalleerd"
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install apache -y
 sudo apt-get install apache2 apache2-utils -y
+sudo apt-get install tcpdump -y
+sudo apt-get install gedit -y
 echo -e " "
 
 
