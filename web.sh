447baed100427ee60bbb1a501a49800b4d4c5e64
opdoffer <rduivesteijn@gmail.com>
2016-12-02 09:57:56 +0100

    uopd

--- web.sh
+++ web.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 ### CBL script installing web-bestanden
 
-echo -e "De website bestanden worden gekopieerd"
 sudo chmod -R 0777 /var/www/html
 sudo cp -r ./cbl-web/www/* /var/www/html
-
+echo -e "De website bestanden zijn gekopieerd"
 echo -e " "
 
 
