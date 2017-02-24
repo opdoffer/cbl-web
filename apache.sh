eebdcea9e1506428231789c4b668ecf870af43bf
opdoffer <remco@MBP-van-Remco.ipool.loc>
2017-02-24 11:04:39 +0100

    --force-yes

--- apache.sh
+++ apache.sh
@@ -4,7 +4,7 @@
 echo -e "Webserver en alle extra's worden geïnstalleerd"
 #sudo apt-get update -y
 #sudo apt-get upgrade -y
-sudo apt-get install apache2 apache2-utils --force-y
+sudo apt-get install apache2 apache2-utils --force-yes
 sudo apt-get install tcpdump -y
 sudo apt-get install gedit -y
 echo -e " "
