67111ebf4bfd9b5de71587fe3a5a744b15c947af
opdoffer <remco@MBP-van-Remco.ipool.loc>
2017-02-24 09:29:15 +0100

    apache2

--- apache-remove.sh
+++ apache-remove.sh
@@ -3,7 +3,6 @@
 
 echo -e "Webserver en website cbl worden verwijderd"
 sudo apt-get update -y
-sudo apt-get remove --purge apache -y
 sudo apt-get remove --purge apache2 apache2-utils -y
 sudo rm -R /var/www/html
 echo -e "Apache en de websitebestanden zijn verwijderd. gedit en tcpdump zijn nog geïnstalleerd gelaten"
--- apache.sh
+++ apache.sh
@@ -4,7 +4,6 @@
 echo -e "Webserver en alle extra's worden geïnstalleerd"
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install apache -y
 sudo apt-get install apache2 apache2-utils -y
 sudo apt-get install tcpdump -y
 sudo apt-get install gedit -y
