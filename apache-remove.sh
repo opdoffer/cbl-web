39b5dba37d7d829aa64f78929d03cea5e1a82111
opdoffer <remco@MacBook-Pro-van-Remco.local>
2017-02-24 09:26:12 +0100

    Apache remove added

---
+++ apache-remove.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+### CBL script installing Apache2
+
+echo -e "Webserver en website cbl worden verwijderd"
+sudo apt-get update -y
+sudo apt-get remove --purge apache -y
+sudo apt-get remove --purge apache2 apache2-utils -y
+sudo rm -R /var/www/html
+echo -e "Apache en de websitebestanden zijn verwijderd. gedit en tcpdump zijn nog geïnstalleerd gelaten"
+sudo apt-get autoremove
+echo -e " "
+
+
