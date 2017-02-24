7edc519e705303f2ae9560d2de43fdd54837e5f5
opdoffer <remco@MBP-van-Remco.ipool.loc>
2017-02-24 11:02:13 +0100

    Update eruit

--- apache-remove.sh
+++ apache-remove.sh
@@ -2,8 +2,8 @@
 ### CBL script installing Apache2
 
 echo -e "Webserver en website cbl worden verwijderd"
-sudo apt-get update -y
-sudo apt-get remove --purge apache2 apache2-utils -y
+#sudo apt-get update -y
+#sudo apt-get remove --purge apache2 apache2-utils -y
 sudo rm -R /var/www/html
 echo -e "Apache en de websitebestanden zijn verwijderd. gedit en tcpdump zijn nog geïnstalleerd gelaten"
 sudo apt-get autoremove
