3658d92a636b55fe1cc026a4e829fbe0991f9fe3
opdoffer <remco@MBP-van-Remco.ipool.loc>
2017-02-24 11:01:04 +0100

    Update uit

--- apache.sh
+++ apache.sh
@@ -2,9 +2,9 @@
 ### CBL script installing Apache2
 
 echo -e "Webserver en alle extra's worden geïnstalleerd"
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install apache2 apache2-utils -y
+#sudo apt-get update -y
+#sudo apt-get upgrade -y
+sudo apt-get install apache2 apache2-utils --force-y
 sudo apt-get install tcpdump -y
 sudo apt-get install gedit -y
 echo -e " "
