f04aef8af84bdfae2f773a0999f14c3fe1debe9f
opdoffer <remco@MacBook-Pro-van-Remco.local>
2017-02-24 12:49:55 +0100

    N

--- apache-remove.sh
+++ apache-remove.sh
@@ -3,7 +3,7 @@
 
 echo -e "Webserver en website cbl worden verwijderd"
 #sudo apt-get update -y
-#sudo apt-get remove --purge apache2 apache2-utils -y
+sudo apt-get remove --purge apache2 apache2-utils -y
 sudo rm -R /var/www/html
 echo -e "Apache en de websitebestanden zijn verwijderd. gedit en tcpdump zijn nog geïnstalleerd gelaten"
 sudo apt-get autoremove
